cf40804bf2df1a63ae3402bdc67851c2aa4f4252
Ramon Bezerra <ramon.bezerra@armateus.com.br>
2019-10-31 17:19:28 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const FormData = require("form-data");
+const axios = require("axios");
+
+//receber um caminho válido nos argumentos
+const PATH = process.argv[2];
+const IMAGE_SERVER_ADDRESS = process.argv[3];
+let images = [];
+
+const directoryPath = path.join(__dirname, PATH);
+
+fs.readdir(directoryPath, async (err, files) => {
+    if(err){
+        return console.log("Diretório não acessível ", err);
+    }
+
+    const promises = files.map(async (file) => {
+        filepath = path.join(__dirname, PATH, file);
+        let form = new FormData();
+        form.append('image', fs.createReadStream(filepath));
+        form.append('descricao', file);
+        await axios
+            .post(IMAGE_SERVER_ADDRESS+"/upload", 
+                form, 
+                {headers: form.getHeaders()})
+            .then(result => {
+                images.push({
+                   old_name: file,
+                   new_name: result.data.Path,
+                   uuid: result.data.UUID
+                });
+            })
+            .catch((err) => {
+                console.log("Falha ao fazer upload do arquivo ", err.message);
+            });
+    });
+
+    await Promise.all(promises);
+
+    fs.writeFile("files.json", JSON.stringify(images), (err) => {
+        if(err) console.log("Erro ao criar arquivo de mapeamento ", err);
+    
+        console.log("Carga finalizada");
+    });
+    
+});
